20714adae71a4be0aa08acc90573ee45b56b911d
Volodius <volodius@proton.me>
2025-02-01 03:22:13 +0500

    missed something else?

---
+++ Content.Client/Svinopoy/Gui.cs
@@ -0,0 +1,10 @@
+using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.Controls;
+//namespace Svinopoy.GUI.Launcher
+namespace Content.Client.Launcher
+{
+    partial class LauncherConnectingGui
+    {
+        private global::Robust.Client.UserInterface.Controls.BoxContainer DiscordDisconnected => this.FindControl<global::Robust.Client.UserInterface.Controls.BoxContainer>("DiscordDisconnected");
+    }
+}
